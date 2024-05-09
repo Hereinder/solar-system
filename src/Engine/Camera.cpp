f9975ad995f579528e75af4dc4148a3eab26f69a
kdudka <plemiona1225@wp.pl>
2024-05-09 23:35:25 +0200

    [15] - camera singletone

--- src/Engine/Camera.cpp
+++ src/Engine/Camera.cpp
@@ -1,107 +1,188 @@
 #include "Camera.hpp"
 
-static CameraModel::Camera* camera;
+#include <shared_mutex>
 
-void CameraModel::init(unsigned int screenWidth, unsigned int screenHeight) {
-    if (!camera)
-        camera = new CameraModel::Camera(screenWidth, screenHeight);
+Camera::Camera() {
+    m_Front = glm::vec3(0.0f, 0.0f, -1.0f);
+    m_MovementSpeed = SPEED;
+    m_MouseSensitivity = SENSITIVITY;
+    m_Zoom = ZOOM;
+    updateCameraVectors();
+    loop = new std::thread(Camera::test, this);
 }
 
-glm::mat4 CameraModel::GetViewMatrix() {
-    if (!camera)
-        std::cout << "[ERROR CameraMdoel]: Did you inited camera? \n";
-    return camera->GetViewMatrix();
+void Camera::SetCursorMode(uint32_t mode) { m_CursorMode = mode; }
+uint32_t Camera::GetCursorMode() { return m_CursorMode; }
+
+void Camera::init(unsigned int screenWidth, unsigned int screenHeight, glm::vec3 position, glm::vec3 up, float yaw,
+                  float pitch) {
+    if (!m_Inited) {
+        m_Inited = true;
+        m_Position = position;
+        m_WorldUp = up;
+        m_Yaw = yaw;
+        m_Pitch = pitch;
+        m_ScreenWidth = screenWidth;
+        m_ScreenHeight = screenHeight;
+        m_CursorMode = GLFW_CURSOR_NORMAL;
+        lastX = m_ScreenWidth / 2.0f;
+        lastY = m_ScreenHeight / 2.0f;
+    } else {
+        std::cout << "INITED ALREADY \n";
+    }
 }
 
-glm::vec3 CameraModel::GetViewPosition() {
-    if (!camera)
-        std::cout << "[ERROR CameraMdoel]: Did you inited camera? \n";
-    return camera->GetViewPosition();
-}
-
-void CameraModel::ProcessKeyboard(Camera_Movement direction, float deltaTime) {
-    if (!camera)
-        std::cout << "[ERROR CameraMdoel]: Did you inited camera? \n";
-    camera->ProcessKeyboard(direction, deltaTime);
-}
-
-void CameraModel::mouse_callback(GLFWwindow*, double xposIn, double yposIn) {
-    if (camera->GetCursorMode() != GLFW_CURSOR_DISABLED) {
-        camera->firstMouse = true;
+void Camera::mouse_callback(GLFWwindow*, double xposIn, double yposIn) {
+    if (GetCursorMode() != GLFW_CURSOR_DISABLED) {
+        firstMouse = true;
         return;
     }
     float xpos = static_cast<float>(xposIn);
     float ypos = static_cast<float>(yposIn);
 
-    if (camera->firstMouse) {
-        camera->lastX = xpos;
-        camera->lastY = ypos;
-        camera->firstMouse = false;
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
     }
 
-    float xoffset = xpos - camera->lastX;
-    float yoffset = camera->lastY - ypos;  // reversed since y-coordinates go from bottom to top
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;  // reversed since y-coordinates go from bottom to top
 
-    camera->lastX = xpos;
-    camera->lastY = ypos;
+    lastX = xpos;
+    lastY = ypos;
 
-    camera->ProcessMouseMovement(xoffset, yoffset);
+    ProcessMouseMovement(xoffset, yoffset);
 }
 
-void CameraModel::scroll_callback(GLFWwindow*, double, double) {}
+void Camera::scroll_callback(GLFWwindow*, double, double) {}
 
-void CameraModel::key_callback(GLFWwindow*, int key, int, int action, int) {
+void Camera::key_callback(GLFWwindow*, int key, int, int action, int) {
     if (key == GLFW_KEY_A && action == GLFW_PRESS) {
-        camera->map[CameraModel::Camera_Movement::LEFT] = true;
+        map[Camera_Movement::LEFT] = true;
     }
     if (key == GLFW_KEY_D && action == GLFW_PRESS) {
-        camera->map[CameraModel::Camera_Movement::RIGHT] = true;
+        map[Camera_Movement::RIGHT] = true;
     }
     if (key == GLFW_KEY_W && action == GLFW_PRESS) {
-        camera->map[CameraModel::Camera_Movement::FORWARD] = true;
+        map[Camera_Movement::FORWARD] = true;
     }
     if (key == GLFW_KEY_S && action == GLFW_PRESS) {
-        camera->map[CameraModel::Camera_Movement::BACKWARD] = true;
+        map[Camera_Movement::BACKWARD] = true;
     }
     if (key == GLFW_KEY_Q && action == GLFW_PRESS) {
-        camera->map[CameraModel::Camera_Movement::UP] = true;
+        map[Camera_Movement::UP] = true;
     }
     if (key == GLFW_KEY_E && action == GLFW_PRESS) {
-        camera->map[CameraModel::Camera_Movement::DOWN] = true;
+        map[Camera_Movement::DOWN] = true;
     }
 
     if (key == GLFW_KEY_A && action == GLFW_RELEASE) {
-        camera->map[CameraModel::Camera_Movement::LEFT] = false;
+        map[Camera_Movement::LEFT] = false;
     }
     if (key == GLFW_KEY_D && action == GLFW_RELEASE) {
-        camera->map[CameraModel::Camera_Movement::RIGHT] = false;
+        map[Camera_Movement::RIGHT] = false;
     }
     if (key == GLFW_KEY_W && action == GLFW_RELEASE) {
-        camera->map[CameraModel::Camera_Movement::FORWARD] = false;
+        map[Camera_Movement::FORWARD] = false;
     }
     if (key == GLFW_KEY_S && action == GLFW_RELEASE) {
-        camera->map[CameraModel::Camera_Movement::BACKWARD] = false;
+        map[Camera_Movement::BACKWARD] = false;
     }
     if (key == GLFW_KEY_Q && action == GLFW_RELEASE) {
-        camera->map[CameraModel::Camera_Movement::UP] = false;
+        map[Camera_Movement::UP] = false;
     }
     if (key == GLFW_KEY_E && action == GLFW_RELEASE) {
-        camera->map[CameraModel::Camera_Movement::DOWN] = false;
+        map[Camera_Movement::DOWN] = false;
     }
 }
 
-void CameraModel::mouse_button_callback(GLFWwindow* window, int button, int action, int) {
+void Camera::mouse_button_callback(GLFWwindow* window, int button, int action, int) {
     if (button == GLFW_MOUSE_BUTTON_1 && action == GLFW_RELEASE) {
-        if (camera->GetCursorMode() != GLFW_CURSOR_NORMAL) {
-            camera->SetCursorMode(GLFW_CURSOR_NORMAL);
+        if (GetCursorMode() != GLFW_CURSOR_NORMAL) {
+            SetCursorMode(GLFW_CURSOR_NORMAL);
             glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
         }
     }
 
     if (button == GLFW_MOUSE_BUTTON_1 && action == GLFW_PRESS) {
-        if (camera->GetCursorMode() != GLFW_CURSOR_DISABLED) {
+        if (GetCursorMode() != GLFW_CURSOR_DISABLED) {
             glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-            camera->SetCursorMode(GLFW_CURSOR_DISABLED);
+            SetCursorMode(GLFW_CURSOR_DISABLED);
+        }
+    }
+}
+
+// processes input received from any keyboard-like input system. Accepts input parameter in the form of camera
+// defined ENUM (to abstract it from windowing systems)
+void Camera::ProcessKeyboard(Camera_Movement direction, float deltaTime) {
+    float velocity = m_MovementSpeed * deltaTime;
+    if (direction == FORWARD)
+        m_Position += m_Front * velocity;
+    if (direction == BACKWARD)
+        m_Position -= m_Front * velocity;
+    if (direction == LEFT)
+        m_Position -= m_Right * velocity;
+    if (direction == RIGHT)
+        m_Position += m_Right * velocity;
+    if (direction == UP)
+        m_Position += m_Up * velocity;
+    if (direction == DOWN)
+        m_Position -= m_Up * velocity;
+}
+
+// // processes input received from a mouse input system. Expects the offset value in both the x and y
+// direction.
+void Camera::ProcessMouseMovement(float xoffset, float yoffset, bool constrainPitch) {
+    xoffset *= m_MouseSensitivity;
+    yoffset *= m_MouseSensitivity;
+
+    m_Yaw += xoffset;
+    m_Pitch += yoffset;
+
+    // make sure that when pitch is out of bounds, screen doesn't get flipped
+    if (constrainPitch) {
+        if (m_Pitch > 89.0f)
+            m_Pitch = 89.0f;
+        if (m_Pitch < -89.0f)
+            m_Pitch = -89.0f;
+    }
+
+    // update m_Front, Right and Up Vectors using the updated Euler angles
+    updateCameraVectors();
+}
+
+// processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+void Camera::ProcessMouseScroll(float yoffset) {
+    m_Zoom -= (float)yoffset;
+    if (m_Zoom < 1.0f)
+        m_Zoom = 1.0f;
+    if (m_Zoom > 45.0f)
+        m_Zoom = 45.0f;
+}
+
+// calculates the front vector from the Camera's (updated) Euler Angles
+void Camera::updateCameraVectors() {
+    // calculate the new Front vector
+    glm::vec3 front;
+    front.x = cos(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
+    front.y = sin(glm::radians(m_Pitch));
+    front.z = sin(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
+    m_Front = glm::normalize(front);
+    // also re-calculate the Right and Up vector
+    m_Right = glm::normalize(
+        glm::cross(m_Front, m_WorldUp));  // normalize the vectors, because their length gets closer to 0
+                                          // the more you look up or down which results in slower movement.
+    m_Up = glm::normalize(glm::cross(m_Right, m_Front));
+}
+
+void Camera::test() {
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        for (auto const& [key, val] : map) {
+            if (val == true) {
+                ProcessKeyboard(key, 2.0f);
+            }
         }
     }
 }
--- src/Engine/Camera.hpp
+++ src/Engine/Camera.hpp
@@ -6,179 +6,83 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <iostream>
+#include <mutex>
 #include <thread>
 #include <unordered_map>
 
-namespace CameraModel {
-    // Defines several possible options for camera movement. Used as abstraction to stay away from window-system
-    // specific input methods
-    enum Camera_Movement { FORWARD, BACKWARD, LEFT, RIGHT, UP, DOWN };
-    // Default camera values
-    const float SPEED = 10.5f;
-    const float SENSITIVITY = 0.15f;
-    const float ZOOM = 45.0f;
-    // An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices
-    // for use in OpenGL
-    class Camera {
-    public:
-        // constructor with vectors
-        Camera(unsigned int screenWidth, unsigned int screenHeight, glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
-               glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = -90.0, float pitch = 0.0)
-            : Front(glm::vec3(0.0f, 0.0f, -1.0f)),
-              MovementSpeed(SPEED),
-              MouseSensitivity(SENSITIVITY),
-              Zoom(ZOOM),
-              m_ScreenWidth(screenWidth),
-              m_ScreenHeight(screenHeight) {
-            Position = position;
-            WorldUp = up;
-            Yaw = yaw;
-            Pitch = pitch;
-            updateCameraVectors();
+// Defines several possible options for camera movement.
+enum Camera_Movement { FORWARD, BACKWARD, LEFT, RIGHT, UP, DOWN };
 
-            lastX = m_ScreenWidth / 2.0f;
-            lastY = m_ScreenHeight / 2.0f;
-            loop = new std::thread(CameraModel::Camera::test, this);
-        }
-        // constructor with scalar values
-        Camera(unsigned int screenWidth, unsigned int screenHeight, float posX, float posY, float posZ, float upX,
-               float upY, float upZ, float yaw, float pitch)
-            : Front(glm::vec3(0.0f, 0.0f, -1.0f)),
-              MovementSpeed(SPEED),
-              MouseSensitivity(SENSITIVITY),
-              Zoom(ZOOM),
-              m_ScreenWidth(screenWidth),
-              m_ScreenHeight(screenHeight) {
-            Position = glm::vec3(posX, posY, posZ);
-            WorldUp = glm::vec3(upX, upY, upZ);
-            Yaw = yaw;
-            Pitch = pitch;
-            updateCameraVectors();
+// Default camera values
+const float SPEED = 5.5f;
+const float SENSITIVITY = 0.15f;
+const float ZOOM = 45.0f;
 
-            lastX = m_ScreenWidth / 2.0f;
-            lastY = m_ScreenHeight / 2.0f;
-            loop = new std::thread(CameraModel::Camera::test, this);
-        }
+class Camera {
+public:
+    // constructor with vectors
+    static Camera& getInstance() {
+        static Camera m_Instance;
+        return m_Instance;
+    }
 
-        // returns the view matrix calculated using Euler Angles and the LookAt Matrix
-        glm::mat4 GetViewMatrix() { return glm::lookAt(Position, Position + Front, Up); }
+    Camera(Camera const&) = delete;
+    void operator=(Camera const&) = delete;
 
-        glm::vec3 GetViewPosition() { return Position; }
+    // returns the view matrix calculated using Euler Angles and the LookAt Matrix
+    glm::mat4 GetViewMatrix() { return glm::lookAt(m_Position, m_Position + m_Front, m_Up); }
+    glm::vec3 GetViewPosition() { return m_Position; }
 
-        // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera
-        // defined ENUM (to abstract it from windowing systems)
-        void ProcessKeyboard(Camera_Movement direction, float deltaTime) {
-            float velocity = MovementSpeed * deltaTime;
-            if (direction == FORWARD)
-                Position += Front * velocity;
-            if (direction == BACKWARD)
-                Position -= Front * velocity;
-            if (direction == LEFT)
-                Position -= Right * velocity;
-            if (direction == RIGHT)
-                Position += Right * velocity;
-            if (direction == UP)
-                Position += Up * velocity;
-            if (direction == DOWN)
-                Position -= Up * velocity;
-        }
-
-        // // processes input received from a mouse input system. Expects the offset value in both the x and y
-        // direction.
-        void ProcessMouseMovement(float xoffset, float yoffset, bool constrainPitch = true) {
-            xoffset *= MouseSensitivity;
-            yoffset *= MouseSensitivity;
-
-            Yaw += xoffset;
-            Pitch += yoffset;
-
-            // make sure that when pitch is out of bounds, screen doesn't get flipped
-            if (constrainPitch) {
-                if (Pitch > 89.0f)
-                    Pitch = 89.0f;
-                if (Pitch < -89.0f)
-                    Pitch = -89.0f;
-            }
-
-            // update Front, Right and Up Vectors using the updated Euler angles
-            updateCameraVectors();
-        }
-
-        // processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
-        void ProcessMouseScroll(float yoffset) {
-            Zoom -= (float)yoffset;
-            if (Zoom < 1.0f)
-                Zoom = 1.0f;
-            if (Zoom > 45.0f)
-                Zoom = 45.0f;
-        }
-
-        void SetCursorMode(uint32_t mode) { m_CursorMode = mode; }
-        uint32_t GetCursorMode() { return m_CursorMode; }
-
-    private:
-        // calculates the front vector from the Camera's (updated) Euler Angles
-        void updateCameraVectors() {
-            // calculate the new Front vector
-            glm::vec3 front;
-            front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-            front.y = sin(glm::radians(Pitch));
-            front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-            Front = glm::normalize(front);
-            // also re-calculate the Right and Up vector
-            Right = glm::normalize(
-                glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more
-                                              // you look up or down which results in slower movement.
-            Up = glm::normalize(glm::cross(Right, Front));
-        }
-
-        void test() {
-            while (true) {
-                std::this_thread::sleep_for(std::chrono::milliseconds(1));
-                for (auto const& [key, val] : map) {
-                    if (val == true) {
-                        ProcessKeyboard(key, 2.0f);
-                    }
-                }
-            }
-        }
-
-    public:
-        // camera Attributes
-        glm::vec3 Position;
-        glm::vec3 Front;
-        glm::vec3 Up;
-        glm::vec3 Right;
-        glm::vec3 WorldUp;
-        // euler Angles
-        float Yaw;
-        float Pitch;
-        // camera options
-        float MovementSpeed;
-        float MouseSensitivity;
-        float Zoom;
-        std::unordered_map<CameraModel::Camera_Movement, bool> map;
-
-        float lastX;
-        float lastY;
-        bool firstMouse = true;
-
-    private:
-        std::thread* loop;
-        unsigned int m_ScreenWidth;
-        unsigned int m_ScreenHeight;
-        uint32_t m_CursorMode = GLFW_CURSOR_NORMAL;
-    };
-
-    void init(unsigned int screenWidth, unsigned int screenHeight);
-    glm::mat4 GetViewMatrix();
-    glm::vec3 GetViewPosition();
-
-    void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+    void init(unsigned int screenWidth, unsigned int screenHeight, glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
+              glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = -90.0, float pitch = 0.0);
 
     void mouse_callback(GLFWwindow* window, double xpos, double ypos);
     void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
     void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
     void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
 
-}
+private:
+    Camera();
+    // Camera(Camera const&);  // Don't Implement
+    // void operator=(Camera const&);
+
+    void SetCursorMode(uint32_t mode);
+    uint32_t GetCursorMode();
+
+    void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+    void ProcessMouseMovement(float xoffset, float yoffset, bool constrainPitch = true);
+    void ProcessMouseScroll(float yoffset);
+
+    // calculates the front vector from the Camera's (updated) Euler Angles
+    void updateCameraVectors();
+    void test();
+
+private:
+    // camera Attributes
+    glm::vec3 m_Position;
+    glm::vec3 m_Front;
+    glm::vec3 m_Up;
+    glm::vec3 m_Right;
+    glm::vec3 m_WorldUp;
+    // euler Angles
+    float m_Yaw;
+    float m_Pitch;
+    // camera options
+    float m_MovementSpeed;
+    float m_MouseSensitivity;
+    float m_Zoom;
+    std::unordered_map<Camera_Movement, bool> map;
+
+    float lastX = 0;
+    float lastY = 0;
+    uint32_t m_CursorMode;
+    bool firstMouse = true;
+
+    std::thread* loop;
+    Camera* m_Instance;
+    std::mutex m_Mutex;
+    bool m_Inited = false;
+
+    unsigned int m_ScreenWidth;
+    unsigned int m_ScreenHeight;
+};
